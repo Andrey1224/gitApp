dd7dc4d1a12a4b75cff2f327c9c37b75667336b9
Andrii Nepodymka <anepodymka@gmail.com>
2021-05-28 12:54:22 -0400

    Initial Commit

---
+++ IAMPoor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAMPoor
+//
+//  Created by Andrey Nepodymka on 5/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
